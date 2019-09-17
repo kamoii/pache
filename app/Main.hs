abbabc0e5c7051c6244e54cf74a499b41ac2cabd
kamoii <>
2019-09-17 21:15:57 +0900

    Started dante

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,28 @@
 module Main where
 
+import Prelude()
+import Relude
+import HieBin
+import NameCache
+import UniqSupply (mkSplitUniqSupply)
+import System.Environment (getArgs)
+
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  [hiePath] <- getArgs
+  nc <- makeNc
+  (hieFile, nc') <- readHieFile nc hiePath
+  putStrLn "hieFile read!"
+
+-- https://gitlab.haskell.org/ghc/ghc/wikis/hie-files#reading-hie-files
+makeNc :: IO NameCache
+makeNc = do
+  uniqSupply <- mkSplitUniqSupply 'z'
+  return $ initNameCache uniqSupply []
+
+-- Local Variables:
+-- dante-target: "exe:pache"
+-- eval: (dante-mode)
+-- eval: (flycheck-mode)
+-- End:
