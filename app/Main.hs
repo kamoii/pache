93334ff5cc61d704fe9697da94feeefb74b6246d
kamoii <>
2019-09-18 00:06:59 +0900

    WIP

--- app/Main.hs
+++ app/Main.hs
@@ -3,17 +3,22 @@ module Main where
 import Prelude()
 import Relude
 import HieBin
-import NameCache
+import HieTypes
+import NameCache (NameCache, initNameCache)
 import UniqSupply (mkSplitUniqSupply)
 import System.Environment (getArgs)
-
+import qualified Data.Map as M
 
 main :: IO ()
 main = do
   [hiePath] <- getArgs
   nc <- makeNc
-  (hieFile, nc') <- readHieFile nc hiePath
-  putStrLn "hieFile read!"
+  (hieFileResult, nc') <- readHieFile nc hiePath
+  let hieFile = hie_file_result hieFileResult
+  let filePath = hie_hs_file hieFile
+  let asts = hie_asts hieFile
+  putStrLn $ "hieFile read!! from: " <> filePath
+  print $ M.keys $ getAsts asts
 
 -- https://gitlab.haskell.org/ghc/ghc/wikis/hie-files#reading-hie-files
 makeNc :: IO NameCache
